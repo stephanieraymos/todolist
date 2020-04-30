d03ba6075c5ca2e450ab2839a2c60d751fbec86d
Stephanie Raymos <stephanieraymos@gmail.com>
2020-04-30 11:46:16 -0700

    Added todos array and added as props to Todos render

--- src/App.js
+++ src/App.js
@@ -1,15 +1,38 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Todos from './Components/Todos';
 import './App.css';
 
-function App() {
-  return (
-    //JSX, Javascript can be used in JSX with {curly braces}
-    <div className="App">
-      <Todos />
-    </div>
-  );
+class App extends Component {
+  state = {
+    todos: [
+      {
+        id: 1,
+        title: "Take out the trash",
+        completed: false
+      },
+      {
+        id: 2,
+        title: "Dinner with wife",
+        completed: false
+      },
+      {
+        id: 3,
+        title: "Meeting with boss",
+        completed: false
+      }
+    ]
+  }
+
+  render() {
+    return (
+      //JSX, Javascript can be used in JSX with {curly braces}
+      <div className="App">
+        <Todos todos= {this.state.todos} />
+      </div>
+    );
+  }
 }
+
 
 export default App;
 
