63961e0cee81a35877dd9b311e45b06379ac9218
Stephanie Raymos <stephanieraymos@gmail.com>
2020-05-03 20:58:01 -0700

    Destructured todo and pulled out id + title in TodoItem

--- src/Components/TodoItem.js
+++ src/Components/TodoItem.js
@@ -13,11 +13,12 @@ export class TodoItem extends Component {
   }
 
   render() {
+    const { id, title } = this.props.todo;
     return (
       <div style={this.getStyle()}>
         <p>
-          <input type="checkbox" onChange={this.props.markComplete.bind(this, this.props.todo.id)} /> {' '}
-          {this.props.todo.title}</p>
+          <input type="checkbox" onChange={this.props.markComplete.bind(this, id)} /> {' '}
+          {title}</p>
       </div>
     )
   }
