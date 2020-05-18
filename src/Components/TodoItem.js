c0792f9e0eba5d9c0c4d15ca228efdc033432d9e
Stephanie Raymos <stephanieraymos@gmail.com>
2020-05-18 15:25:17 -0700

    Moved btnStyle into stylesheet

--- src/Components/TodoItem.js
+++ src/Components/TodoItem.js
@@ -19,7 +19,7 @@ export class TodoItem extends Component {
         <p>
           <input type="checkbox" onChange={this.props.markComplete.bind(this, id)} /> {' '}
           {title}
-          <button onClick={this.props.delTodo.bind(this,id)} style={btnStyle}>x</button>
+          <button onClick={this.props.delTodo.bind(this,id)} className="btnStyle">x</button>
           </p>
       </div>
     )
@@ -31,15 +31,6 @@ TodoItem.propTypes = {
   todo: PropTypes.object.isRequired
 }
 
-const btnStyle = {
-  background:'#ff0000',
-  color: '#fff',
-  border: 'none',
-  padding: '5px 9px',
-  borderRadius: '50%',
-  cursor: 'pointer',
-  float: 'right'
-}
 
 export default TodoItem
 
