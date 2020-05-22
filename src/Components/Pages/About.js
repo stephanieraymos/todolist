4bacc1c7f09a945a945d0ae5ef0d9003e70a9e24
Stephanie Raymos <stephanieraymos@gmail.com>
2020-05-22 11:01:01 -0700

    Created About.js + installed react router dom

---
+++ src/Components/Pages/About.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function About() {
+  return (
+    <React.Fragment>
+      <h1>About</h1>
+      <p>This is the todo list app version 1.00.</p>
+    </React.Fragment>
+  )
+}
+
+
+export default About;
